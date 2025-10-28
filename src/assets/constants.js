437ad1726aaff50bd2a7a48b3a18fa999af70af9
Natasha Bagde <124035587+bagdenatasha2001@users.noreply.github.com>
2025-10-28 17:08:33 +0530

    Update constants.js

--- src/assets/constants.js
+++ src/assets/constants.js
@@ -1,4 +1,3 @@
-//  export const URL="https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=AIzaSyDgCMV8_BcJDgjqpfu0TeoKuM8Y1u9kq2Q"
 
 const API_KEY = import.meta.env.VITE_API_KEY;
 
