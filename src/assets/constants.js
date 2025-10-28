f734750fa9d5f9b1b78dadf6304e94d4af877c27
Mohit Bagde <natashabagde692@gmail.com>
2025-10-28 17:06:56 +0530

    Moved API key to .ene file

--- src/assets/constants.js
+++ src/assets/constants.js
@@ -1,1 +1,5 @@
- export const URL="https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=AIzaSyDgCMV8_BcJDgjqpfu0TeoKuM8Y1u9kq2Q"
+//  export const URL="https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=AIzaSyDgCMV8_BcJDgjqpfu0TeoKuM8Y1u9kq2Q"
+
+const API_KEY = import.meta.env.VITE_API_KEY;
+
+export const URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${API_KEY}`;
